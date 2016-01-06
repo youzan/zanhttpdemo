f917fc8a695e0eab96614afa41691935502e73c9
winglechen <winglechen@gmail.com>
2016-01-06 11:51:23 +0800

    add bootstrap

---
+++ init/Bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+if(defined('APP_BOOTSTRAP')){
+        return ;
+}
+define('APP_BOOTSTRAP','bootstrap');
+
+
+define('ROOT_PATH', __DIR__ . '/../');
+
+require __DIR__ . '/../vendor/autoload.php';
+
+
