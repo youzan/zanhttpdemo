aa35a08093ad960510ca79e0f0cc7804abb70fcb
winglechen <winglechen@gmail.com>
2016-02-29 16:40:17 +0800

    rename some dir

---
+++ init/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+define('ROOT_PATH', __DIR__ . '/../');
+
+require __DIR__ . '/../vendor/autoload.php';
+
+
+$appConfig = [
+    'appName'           => 'zanweb',
+    'appPath'           => ROOT_PATH,
+];
+
+
+return $appConfig;
+
