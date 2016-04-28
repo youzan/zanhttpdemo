b856f38ab66961000d2c02f0c506d42287c5f723
panweikang <panweikang@youzan.com>
2016-04-28 16:35:42 +0800

    delete dev-feature/worker_monitor

--- resource/config/test/server.php
+++ resource/config/test/server.php
@@ -9,14 +9,12 @@
 return [
     'host'          => '127.0.0.1',
     'port'          => '8030',
-    'config' => [
-        'worker_num'    => 2,
-    ],
-    'monitor' =>[
-        'reaction_limit' => 100000,     //
-        'live_time' => 1800000,         //30m
-        'check_time' => 1000,           //1s
-        'memory_limit' => 1610612736,   //1.5G
-        'cpu_limit' => 70,
-    ]
+    'worker_num'    => 2,
+//    'monitor' =>[
+//        'reaction_limit' => 100000,     //
+//        'live_time' => 1800000,         //30m
+//        'check_time' => 1000,           //1s
+//        'memory_limit' => 1610612736,   //1.5G
+//        'cpu_limit' => 70,
+//    ]
 ];
