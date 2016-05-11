44796d2e4bff2a3af88b6106a2312357beeb17f2
小牛 <xiaoniu@qima-inc.com>
2016-05-11 11:30:00 +0800

    sql map

--- resource/config/test/connection/mysql.php
+++ resource/config/test/connection/mysql.php
@@ -8,18 +8,21 @@
 return [
     'default_write' => [
         'engine'=> 'mysqli',
+        'host' => '192.168.66.202',
+        'user' => 'test_koudaitong',
+        'password' => 'nPMj9WWpZr4zNmjz',
+        'database' => 'pf',
+        'port' => '3306',
         'pool'  => [
             'pool_name' => 'pifa',
             'maximum-connection-count' => '50',
             'minimum-connection-count' => '10',
-            'keeping-sleep-time' => '10',
             'heartbeat-time' => 35000,
-            'init-connection'=> '2',
-            'host' => '192.168.66.202',
-            'user' => 'test_koudaitong',
-            'password' => 'nPMj9WWpZr4zNmjz',
-            'database' => 'pf',
-            'port' => '3306'
+            'init-connection'=> '5',
         ],
+    ],
+    'default_read' => [
+        'engine'=> 'mysqli',
+        'pool'  => [],
     ],
 ];
